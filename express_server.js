965bbd1de9fcda7dd01466fecdb3b7df2bcb0067
Shashi Bawa <shashibawa104@gmail.com>
2021-12-07 21:20:57 +0000

    Saved data to the database and redirected to another page

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
-function generateRandomString() {
-   
-    return  Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);  
-   }const express = require("express");
+const randomStr = function generateRandomString() {
+   return  Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);  
+}
+const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -31,8 +34,10 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 app.post("/urls", (req, res) => {
+    let shortURL = randomStr();
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`/urls/${shortURL}`);
     console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };

03d6163319d6b110818a2818e13d5d4ea60e5334
Shashi Bawa <shashibawa104@gmail.com>
2021-12-08 05:16:55 +0000

    Add route to delete urls

--- express_server.js
+++ express_server.js
@@ -46,7 +46,12 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
      const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
-  });
+});
+app.post("/urls/:shortURL/delete", (req,res) => {
+    let shortURL = req.params.shortURL;
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

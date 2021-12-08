be694bbd4f73e7c1a82b4b331bb7fe56f5beb868
Shashi Bawa <shashibawa104@gmail.com>
2021-12-08 07:00:20 +0000

    Added route for updaing URLS

--- express_server.js
+++ express_server.js
@@ -47,10 +47,19 @@ app.get("/u/:shortURL", (req, res) => {
      const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
 });
+app.post("/urls/:shortURL/edit", (req,res) => {
+    let shortURL = req.params.shortURL;
+    res.redirect(`/urls/${shortURL}`);
+});
+app.post("/urls/:shortURL/edited", (req, res) => {
+   let shortURL = req.params.shortURL;
+   urlDatabase[shortURL] = req.body.longURL;
+    res.redirect("/urls");    
+});
 app.post("/urls/:shortURL/delete", (req,res) => {
     let shortURL = req.params.shortURL;
     delete urlDatabase[shortURL];
-    res.redirect("/urls");
+    res.redirect(`/urls`);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

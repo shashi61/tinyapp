908d03426712714b00d7d41e3c21c362cdf6b4a2
Shashi Bawa <shashibawa104@gmail.com>
2021-12-08 21:09:27 +0000

    Added logout functionality using /logout route

--- express_server.js
+++ express_server.js
@@ -72,6 +72,10 @@ app.post('/login', (req,res) => {
     res.cookie('username', username);
     res.redirect('/urls');
 })
+app.post('/logout', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('urls');
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
